a3f59c0bda12f63acc52f07866ea9ea28798a6c7
em4n0n <58372945+em4n0n@users.noreply.github.com>
2023-03-27 00:10:42 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+
+
+import pandas as pd
+import yfinance as yf
+import requests
+from bs4 import BeautifulSoup
+
+def get_sp500_instruments():
+    res = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
+    soup = BeautifulSoup(res.content,'lxml')
+    table = soup.find_all('table')[0] 
+    df = pd.read_html(str(table))
+    return list(df[0]["Symbol"])
+
+tickers = get_sp500_instruments()
+print(tickers)
